1f458101bf84b1b606d95318ff4b9a52ab351f07
jco1 <jco1@dtcc.edu>
2016-01-19 16:04:07 -0500

    Idk

--- src/p2/Main.java
+++ src/p2/Main.java
@@ -10,7 +10,18 @@ public class Main
     public static void main(String[] args)
     {
         //initializing array/size
-	    double[] monthlySales = new double[12];
+	    double[][] grades = new double[4][3];
+        String[] names = new String[]{"John", "Mac", "Papi", "Boul"};
+
+        //Initializes avg grades variables
+        double avgGrade1 = 0;
+        double avgGrade2 = 0;
+        double avgGrade3 = 0;
+        double avgGrade4 = 0;
+
+        double avgTest1 = 0;
+        double avgTest2 = 0;
+        double avgTest3 = 0;
 
         //Initializes random generator
         Random ran = new Random();
@@ -21,20 +32,53 @@ public class Main
         //Sets the formatter to round up
         df.setRoundingMode(RoundingMode.CEILING);
 
-        for (int x = 0; x < 12; x++)
+        for (int x = 0; x < 4; x++)
         {
-            //defines each array position using random number generator
-            monthlySales[x] = 0 + (10000 - 0) * ran.nextDouble();
+            for (int y = 0; y < 3; y++)
+
+            {
+                //defines each array position using random number generator
+                grades[x][y] = 70 + (100 - 70) * ran.nextDouble();
+
+            }
+
+        } //Ends Initial For Loop
+
+        for (int i = 0; i < 3; i++)
+
+        {
+            //Adds the three test grades
+            avgGrade1 = grades[0][i] + avgGrade1;
+            avgGrade2 = grades[1][i] + avgGrade2;
+            avgGrade3 = grades[2][i] + avgGrade3;
+            avgGrade4 = grades[3][i] + avgGrade4;
 
         }
 
-        for (double each : monthlySales)
-        {
-            //prints the array in format
-            System.out.println(df.format(each));
+        double[] array = new double[]{avgGrade1, avgGrade2, avgGrade3, avgGrade4};
 
+        for (int x = 0; x < 4; x++)
+        {
+           System.out.println(names[x] + "'s average grade is: " + df.format(array[x]/3));
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            avgTest1 = grades[i][0] + avgTest1;
+            avgTest2 = grades[i][1] + avgTest2;
+            avgTest3 = grades[i][2] + avgTest3;
+
+        }
+
+        double[] arrayTest = new double[]{avgTest1, avgTest2, avgTest3};
+
+        String[] testNames = new String[]{"Test 1", "Test 2", "Test 3"};
+
+        for (int x = 0; x < 3; x++)
+        {
+            System.out.println(testNames[x] + "'s average grade is: " + df.format(arrayTest[x]/4));
         }
 
     }
 
-}
+}//Ends Main
